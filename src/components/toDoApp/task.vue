bb31c2bf23ef039a45192d9b05f78d1039fc5fcc
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2020-12-17 16:31:51 -0500

    task now uses radionput

--- src/components/toDoApp/task.vue
+++ src/components/toDoApp/task.vue
@@ -1,25 +1,25 @@
 <template>
   <section>
-    <h3>
-      {{ todo }}
-    </h3>
-    <label>
-      <input
-        type="checkbox"
-        :checked="completed"
-        @change="toggleCompletion()"
-      />
-      {{ completed }}
-    </label>
+    <radio-input
+      :label="todo"
+      :name="`task-${id}`"
+      :value="completed"
+      :inline="booleanTrue"
+      @selected="val => toggleCompletion(val)"
+      @alerts="alerts"
+    />
+    <span class="fas fa-minus-square" @click.prevent="toggleCompletion()" />
   </section>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 import { useStore } from "@/store";
 import { MutationType } from "@/store/mutations";
+import radioInput from "../form/radioInput.vue";
 
 export default defineComponent({
+  components: { radioInput },
   props: {
     id: {
       type: Number,
@@ -37,17 +37,47 @@ export default defineComponent({
 
   setup(props) {
     const store = useStore();
+    const booleanTrue = ref(true);
 
-    const toggleCompletion = () => {
+    const toggleCompletion = (completed: boolean): void => {
       store.commit(MutationType.CompleteItem, {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         id: props.id!,
-        completed: !props.completed
+        completed: completed
       });
     };
 
-    return { toggleCompletion };
+    //used to trigger events if component throws an error
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    let dWarning = null;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    let dDanger = null;
+    const alerts = function(type: string, message: string): void {
+      if (type == "warning") {
+        dWarning = message;
+      } else if (type == "error") {
+        dDanger = message;
+      } else {
+        alert("error in input alert module");
+      }
+    }; //alerts
+
+    return { toggleCompletion, alerts, booleanTrue };
   }
 });
 </script>
-<style lang="less"></style>
+<style lang="less">
+section {
+  display: flex;
+  // & > label {
+  //   padding: 8px 16px;
+  //   border: 1px solid black;
+  //   border-radius: 4px;
+  //   display: flex;
+  //   flex-direction: row;
+  //   flex-wrap: nowrap;
+  //   justify-content: center;
+  //   align-content: center;
+  // }
+}
+</style>
