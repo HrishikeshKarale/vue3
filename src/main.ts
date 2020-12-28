af1f58f9e3be1c267756bffce1ecfa2b7e3a34c7
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2020-12-28 12:03:53 -0500

    css/less files added

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import '@/less/bootstrap.less'
+import '@/less/fontAwesome/fontawesome.less'
 
 createApp(App)
   .use(store)
